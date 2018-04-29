271fa9ebd02fc9aa905390bb3340bb76e2a1b8b4
Tristan Dubé <tristan.dube@gmail.com>
2018-04-29 11:16:53 -0400

    Adding the GL libs.
    Test code to make sure everything compiles. Have to copy the
    libtech.dll because it is not automatically copied to the executable
    running folder.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
 #include <libtech/mytime.h>
 #include <libtech/geometry.h>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 
 #define LIBTECH_LOADDLL
 
 int main(int argc, char** argv)
 {
     double d = deg2rad(22);
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        //std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        //std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+
 
     return 0;
 }
