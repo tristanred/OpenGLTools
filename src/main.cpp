37e5adeeac8ed3887e424ffe066abf0d3b34f1a5
Tristan Dubé <tristan.dube@gmail.com>
2018-04-29 10:27:03 -0400

    Initial commit of OpenGLTools.
    This project will be a platform for experimenting with OpenGL.
    
    Linking the project with LibTech as a submodule.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <libtech/mytime.h>
+#include <libtech/geometry.h>
+#include <stdio.h>
+
+#define LIBTECH_LOADDLL
+
+int main(int argc, char** argv)
+{
+    double d = deg2rad(22);
+
+    return 0;
+}
